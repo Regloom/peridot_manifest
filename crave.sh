f9535374f67a1fb81e19a6138c2cfb255cf3b4a7
EgorT <egor.tokarev@outlook.com>
2025-08-28 08:47:20 +0200

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -3,7 +3,6 @@
 
 rm -rf .repo/local_manifests/
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf .repo/project-objects/ .repo/projects/ #repo cache clean
 
 # Repo Init
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
@@ -28,10 +27,6 @@ echo "============================"
 # Cleanup unused HAL display, media, audio
 rm -rf hardware/qcom-caf/{msm8996,msm8998,sdm845,sm8150,sm8250}/{display,media,audio}
 rm -rf hardware/qcom-caf/sm8750
-
-# Empty build-manifest.xml to avoid missing hals error
-mkdir -p out/target/product/peridot/product/etc/
-echo '<?xml version="1.0" encoding="UTF-8"?><manifest></manifest>' > out/target/product/peridot/product/etc/build-manifest.xml
 
 # Export
 export BUILD_USERNAME=regloom
