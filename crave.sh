77544dcecd37d13391dc7b136d0369014cce4ccb
Egor Tokarev <egor.tokarev@outlook.com>
2025-10-24 14:57:13 +0200

    disable submodules initialization

--- crave.sh
+++ crave.sh
@@ -26,16 +26,16 @@ echo "============================"
 echo "============================"
 
 # KernelSU Next setup
-cd kernel/xiaomi/sm8635
-echo "======== Inside kernel/xiaomi/sm8635 ========"
-# curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-# echo "======== Added KSU successfully ========"
-git submodule update --init
-echo "======== Initialized submodules ========"
-# Modify SELinux path in Baseband-guard/sepatch.txt 
-sed -i 's|SELINUX_PATH := \$(shell dirname \$(abspath \$(lastword \$(MAKEFILE_LIST))))|SELINUX_PATH := /tmp/src/android/kernel/xiaomi/sm8635/security/selinux|' Baseband-guard/sepatch.txt
-cd ../../..
-echo "======== Returned to root directory ========"
+# cd kernel/xiaomi/sm8635
+# echo "======== Inside kernel/xiaomi/sm8635 ========"
+# # curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+# # echo "======== Added KSU successfully ========"
+# git submodule update --init
+# echo "======== Initialized submodules ========"
+# # Modify SELinux path in Baseband-guard/sepatch.txt 
+# sed -i 's|SELINUX_PATH := \$(shell dirname \$(abspath \$(lastword \$(MAKEFILE_LIST))))|SELINUX_PATH := /tmp/src/android/kernel/xiaomi/sm8635/security/selinux|' Baseband-guard/sepatch.txt
+# cd ../../..
+# echo "======== Returned to root directory ========"
 
 # Cleanup unused HAL display, media, audio
 # rm -rf hardware/qcom-caf/{msm8996,msm8998,sdm845,sm8150,sm8250}/{display,media,audio}
